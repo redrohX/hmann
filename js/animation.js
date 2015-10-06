26a5a37c5e105f84e06feaa15d26e0df2f1260d4
Niels van Rongen <niels.vanrongen@kega.nl>
2015-10-06 16:44:22 +0200

    +: Added own function to Tween

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,29 @@
+var test = function (el) {
+  var el = el.find('> div'),
+      image = el.data('src');
+  setTimeout(function () {
+    el.css({
+      'background-image' : 'url(' + image + ')',
+      'height' : '350px'
+    });
+    el.addClass('is-visible');
+  }, 750);
+}
+
 // Tween Animation Setup
 // .to('@target', @length, {@object})
 var tween = TweenMax.to(['#animate1', '#animate2'], 0.5, {
   alpha: 1,
-  y: -30,
-  ease: Linear.easeNone
+  y: -100,
+  // ease: Linear.easeNone
 });
 
 // Tween Animation Setup
 // .to('@target', @length, {@object})
-var grow = TweenMax.to(['#animate3'], 0.75, {
+var grow = TweenMax.to(['#animate3'], 0.5, {
   y: 300,
   alpha: 1,
-  ease: Linear.easeNone
+  //ease: Linear.easeNone
 });
 
 // init controller
@@ -28,9 +40,13 @@ var tween_scene = new ScrollMagic.Scene({
 
 // build scene
 var grow_scene = new ScrollMagic.Scene({
-  triggerElement: "#trigger2"
+  triggerElement: "#trigger2",
+  reverse: false
 })
 .setTween(grow) // trigger a TweenMax.to tween
 .triggerHook(1)
 .addIndicators({name: "2 - move photo"}) // add indicators (requires plugin)
+.on('start', function (event) {
+  test($('.widget01--container--more-info--image'));
+})
 .addTo(controller);
