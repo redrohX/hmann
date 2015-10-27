4aeaa27f28caa040ead3a1f965c4b61a4d508224
ricky.wils <ricky.wils@kega.nl>
2015-10-27 13:22:56 +0100

    +: hide indicators to activate tweens

--- js/widget-events-animation.js
+++ js/widget-events-animation.js
@@ -17,7 +17,7 @@ var slide_down_scene = new ScrollMagic.Scene({
 })
     // trigger animation Velocity
     .setTween(".overlay-container-1", 0.5, {alpha: 1, y: 400})
-    .addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+    //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
     .addTo(controller)
     .triggerHook(0.30);
 
@@ -26,6 +26,6 @@ var slide_up_scene = new ScrollMagic.Scene({
 })
     // trigger animation Velocity
     .setTween(".overlay-container-2", 0.5, {alpha: 1, y: -350})
-    .addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+    //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
     .addTo(controller)
     .triggerHook(0.30);
