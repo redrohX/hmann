93d8cdcbddf12db44eeb999c0ebb99422c9a81bb
ricky.wils <ricky.wils@kega.nl>
2015-10-27 11:21:38 +0100

    +: Merged widget JS

--- js/widget01-animation.js
+++ js/widget01-animation.js
@@ -50,3 +50,30 @@ var grow_scene = new ScrollMagic.Scene({
   showImage($('.widget01--container--more-info--image'));
 })
 .addTo(controller);
+
+var slide_big_down_scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger-widget3"
+})
+    // trigger animation Velocity
+    .setTween(".overlay-container", 0.5, {alpha: 1, y: 400})
+    //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+    .addTo(controller)
+    .triggerHook(0.75);
+
+var slide_down_scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger-widget4"
+})
+    // trigger animation Velocity
+    .setTween(".overlay-container-1", 0.5, {alpha: 1, y: 400})
+    .addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+    .addTo(controller)
+    .triggerHook(0.30);
+
+var slide_up_scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger-widget4"
+})
+    // trigger animation Velocity
+    .setTween(".overlay-container-2", 0.5, {alpha: 1, y: -350})
+    .addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
+    .addTo(controller)
+    .triggerHook(0.30);
