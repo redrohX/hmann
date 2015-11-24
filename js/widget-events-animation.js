28567cbca0913e20085e949b906143acfdd9b478
Niels van Rongen <niels.vanrongen@kega.nl>
2015-11-24 16:04:25 +0100

    +: Optimizing widget event mirror in look and feel

--- js/widget-events-animation.js
+++ js/widget-events-animation.js
@@ -2,7 +2,7 @@ var slide_big_down_scene = new ScrollMagic.Scene({
     triggerElement: "#trigger-widget3"
 })
     // trigger animation Velocity
-    .setTween(".overlay-container", 0.5, {alpha: 1, y: 400})
+    .setTween(".overlay-container", 0.5, {alpha: 1, y: 350})
     //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
     .addTo(controller)
     .triggerHook(0.75);
@@ -15,17 +15,20 @@ var slide_big_down_scene = new ScrollMagic.Scene({
 var slide_down_scene = new ScrollMagic.Scene({
     triggerElement: "#trigger-widget4"
 })
-    // trigger animation Velocity
-    .setTween(".overlay-container-1", 0.5, {alpha: 1, y: 400})
-    //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
-    .addTo(controller)
-    .triggerHook(0.30);
+// trigger animation Velocity
+.setTween(".overlay-container-1", 0.5, {alpha: 1, y: 100})
+.addIndicators({name: "event menu"}) // add indicators (requires plugin)
+.addTo(controller)
+.triggerHook(0.90);
 
 var slide_up_scene = new ScrollMagic.Scene({
     triggerElement: "#trigger-widget4"
 })
-    // trigger animation Velocity
-    .setTween(".overlay-container-2", 0.5, {alpha: 1, y: -350})
-    //.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
-    .addTo(controller)
-    .triggerHook(0.30);
+// trigger animation Velocity
+.setTween(".overlay-container-2", 0.5, {
+  alpha: 1,
+  y: -100
+})
+.addIndicators({name: "event menu mirrored"}) // add indicators (requires plugin)
+.addTo(controller)
+.triggerHook(0.40);
