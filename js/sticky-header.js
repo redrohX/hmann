027055890ccffbd68e8acf8be4dc11fdec1d746c
Niels van Rongen <niels.vanrongen@kega.nl>
2015-11-24 11:15:17 +0100

    +: Sticky header without using scrollmagic

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,11 +1,36 @@
 // build scene
-var stickyHeader = new ScrollMagic.Scene({
-  offset: '80',
-  duration: '0'
-})
-.setPin(".nav", {
-    //pushFollowers: true
-})
-.addIndicators({name: "sticky-nav"}) // add indicators (requires plugin)
-.addTo(controller)
-.setClassToggle(".nav", 'nav--sticky');
+// var stickyHeader = new ScrollMagic.Scene({
+//   offset: '80',
+//   duration: '0'
+// })
+// .setPin(".nav", {
+//     //pushFollowers: true
+// })
+// .addIndicators({name: "sticky-nav"}) // add indicators (requires plugin)
+// .addTo(controller)
+// .setClassToggle(".nav", 'nav--sticky');
+
+
+$(function() {
+    var header = $(".header--wrapper");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 80) {
+            header.addClass("header--wrapper--sticky");
+            console.log("sticky")
+            headerAnim.play()
+        } else {
+            header.removeClass("header--wrapper--sticky");
+            console.log("notsticky")
+            headerAnim.reverse()
+       }
+    });
+});
+
+
+var headerAnim = new TimelineLite({paused:true});
+headerAnim.to(".nav", 1, {
+  color:'#fff',
+  ease:Back.easeOut
+});
